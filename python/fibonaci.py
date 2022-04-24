1e6e84fdcafc0e55dc0d1f9e98f27b41122b6759
panggrayta dido <d1do.pankid@gmail.com>
2022-04-24 15:02:39 +0700

    new algorithm

---
+++ python/fibonaci.py
@@ -0,0 +1,17 @@
+# Program to display the Fibonacci sequence up to n-th term
+nterms=10
+# first two terms
+n1, n2 = 0, 1
+count = 0
+
+# check if the number of terms is valid
+print("Fibonacci sequence:")
+while count < nterms:
+   print(n1)
+   nth = n1 + n2
+   # update values
+   n1 = n2 #swap(tukar) value
+   n2 = nth #swap(tukar) value
+   count += 1
+    
+    
